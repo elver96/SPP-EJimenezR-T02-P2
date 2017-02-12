37a2233235a8379174bd2ab06c83d72f0643f3ce
elver <elver@192.168.1.75>
2017-02-12 12:05:05 -0600

    Tarea 2 problema 2

--- src/spp/ejimenez/t2p2/SPPEJimenezT2P2.java
+++ src/spp/ejimenez/t2p2/SPPEJimenezT2P2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package spp.ejimenez.t2p2;
-
+import java.util.Scanner;
 /**
  *
  * @author elver
@@ -15,7 +15,38 @@ public class SPPEJimenezT2P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int valinf, valsup;
+        double res;
+        Scanner kb = new Scanner (System.in);
+        System.out.println("Introduir el valor inferior");
+        valinf= kb.nextInt();
+        System.out.println("Introducir el valor superior");
+        valsup= kb.nextInt();
+    do{
+            valinf ++;
+            res = valinf% 2;
+            if (res>0 || res<0){
+                System.out.println(valinf+" no es divisible entre 2");
+            }
+            else{
+                System.out.println(valinf+" si es divisible entre 2");
+            }
+            res=valinf % 3;
+            if (res>0 || res<0){
+                System.out.println(valinf+" no es divisible entre 3");
+            }
+            else{
+                System.out.println(valinf+" si es divisible entre 3");
+            }
+            res=valinf % 5;
+            if(res>0 || res<0){
+                System.out.println(valinf+" no es divisible entre 5");
+            }
+            else{
+                System.out.println(valinf+" si es divisible entre 5");
+            }
+        }
+        while(valinf< valsup);
     }
     
 }
